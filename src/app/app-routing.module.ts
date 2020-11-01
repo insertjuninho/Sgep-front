57501f9ae81c5cbb9e40a17fd1b126836cb1997e
Juninho <juniorsax3@gmail.com>
2020-11-01 05:21:30 -0300

    sgep-01- criadas model, componentes e routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntradaComponent } from './components/entrada/entrada.component';
+import { HomeLogadoComponent } from './components/home-logado/home-logado.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SaidaComponent } from './components/saida/saida.component';
+import { SolicitarDispensaComponent } from './components/solicitar-dispensa/solicitar-dispensa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeLogadoComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+{
+    path: 'entrada',
+    component: EntradaComponent
+  }, 
+  {
+    path: 'saida',
+    component: SaidaComponent
+  },
+  {
+    path: 'solicitar-dispensa',
+    component: SolicitarDispensaComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/components/home-logado/home-logado.component.ts
+++ src/app/components/home-logado/home-logado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'home-logado',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeLogadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
